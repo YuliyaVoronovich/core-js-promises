acb4b1bd680907c0a5181c2d6ef4b5e5d89bb318
YuliyaVoronovich <Y-1306@mail.ru>
2024-04-07 08:20:06 +0300

    feat: add solution queuPromises

--- src/core-js-promises-tasks.js
+++ src/core-js-promises-tasks.js
@@ -138,8 +138,10 @@ function getAllResult(promises) {
  * [promise1, promise4, promise3] => Promise.resolved('104030')
  * [promise1, promise4, promise3, promise2] => Promise.resolved('10403020')
  */
-function queuPromises(/* promises */) {
-  throw new Error('Not implemented');
+function queuPromises(promises) {
+  return promises.reduce(async (prev, next) => {
+    return (await prev) + (await next);
+  }, '');
 }
 
 module.exports = {
